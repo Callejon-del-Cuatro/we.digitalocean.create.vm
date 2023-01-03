fada5cf1d4bffac21fd70036310e68c4b0d2744b
Rafael García Álvarez <vattenbit@gmail.com>
2023-01-03 23:52:28 +0100

    Add Project for attach doplet

--- create_vm.tf
+++ create_vm.tf
@@ -1,12 +1,17 @@
-# Configure the DigitalOcean provider
-provider "digitalocean" {
-  token = "${var.do_token}"
+data "digitalocean_project" "project" {
+  name = "Workflow Engine"
 }
 
-# Create a new droplet
 resource "digitalocean_droplet" "vm" {
-  name              = "${var.name}"
-  region            = "${var.region}"
-  size              = "${var.size}"
-  image             = "${var.image}"
+  name = "${var.name}"
+  region = "${var.region}"
+  size = "${var.size}"
+  image = "${var.image}"
+}
+
+resource "digitalocean_project_resources" "project" {
+  project = data.digitalocean_project.project.id
+  resources = [
+    digitalocean_droplet.vm.urn
+  ]
 }
